30a56fc514e2a1e5092e022d71c06097c3d648e7
James Scheiber <jscheiber22@gmail.com>
2020-09-19 08:10:02 -0700

    Should be done? i mean except adding in name of who is being watched :)

--- start.py
+++ start.py
@@ -3,8 +3,8 @@ from lister import Lister
 from time import sleep
 
 while True:
-    # Up to ten bots represents Twitch's maximum counted views per IP adress
-    bots = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+    # Up to three bots represents Twitch's maximum counted views per IP adress
+    bots = ["one", "two"]
 
     try:
         users = Lister().returnList()
@@ -18,11 +18,9 @@ while True:
         live = bots[userCount].checkLive()
         if not live:
             bots[userCount].quit()
-            userCount += 1
-        else: # Other attempt failed so this one at least grants 3 views
+        else: # Other attempt failed so this one at least grants 2 :( views
             bots[userCount + 1] = TwitchJoinBot(user)
             userCount += 2
-        if userCount >= 2:
             break
 
     # Waits 30 minutes to check for live again
